9e6e335d5d2d52f483dbb744623fdd4bca61cc93
Evgenij Prishchepov <prishchepovee@gmail.com>
2023-05-06 17:29:01 +0300

    hw-10

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,4 @@
-API_URL = 'https://restcountries.com/v3.1/name/';
+const API_URL = 'https://restcountries.com/v3.1/name/';
 import Notiflix from 'notiflix';
 
 export function fetchCountries(nameOfCountry) {
